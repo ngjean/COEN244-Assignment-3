9c95c7a90496550cfa4d656febf82ef9af02e7be
gabrielarturo94 <36458153+gabrielarturo94@users.noreply.github.com>
2018-03-03 10:21:08 -0500

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -22,6 +22,15 @@ Customer::Customer(int inum, string sname, string saddress, string stel)
 	tel = stel;
 }
 
+Customer::Customer(const Customer &c1){
+	
+	num=c1.num;
+	name=c1.name;
+	address=c1.address;
+	cel=c1.cel;
+	
+}
+
 void Customer::print()const
 {
 	cout<<"Customer#: "<<num<<endl
