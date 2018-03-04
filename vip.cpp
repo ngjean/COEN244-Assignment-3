1f8d160fd8fa401f34822c215c1a878086dcf637
gabrielarturo94 <36458153+gabrielarturo94@users.noreply.github.com>
2018-03-03 23:55:00 -0500

    Update vip.cpp

--- vip.cpp
+++ vip.cpp
@@ -12,15 +12,13 @@ VIP::VIP() : Customer()
 	privilege = 45;
 }
 
-VIP::VIP(int inum, string sname, string saddress, string stell,int iprivilege) : Customer(inum,sname,saddress,stell)
+VIP::VIP(int inum, string sname, string saddress, string stell,int iprivilege) : Customer(inum,sname,saddress,stell,iprivilege)
 {
 	type = "VIP";
-	privilege = iprivilege;
 }
 
 VIP::VIP(const VIP& v1) : Customer(v1){
 
-	privilege = v1.privilege;
 }
 
 void VIP::print()const
